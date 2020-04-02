5775f4f09773968b3be37dff08d5de92c900954f
Michael Anderson <drmikeando@gmail.com>
2020-04-02 10:11:14 +0800

    Switch to using jemalloc in core.

--- markovian-core/src/lib.rs
+++ markovian-core/src/lib.rs
@@ -5,3 +5,9 @@ pub mod sequence_map;
 pub mod substrings;
 pub mod symbol;
 pub mod weighted_sampler;
+
+
+extern crate jemallocator;
+
+#[global_allocator]
+static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
